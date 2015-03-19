1d04b7878ec14b07c2047fc4ee794a453423ee15
Dmitriy Bogolyubov <dmitriy.bogolubov@gmail.com>
2015-03-19 15:08:43 +0300

    Update routes and add load and parse lazada pages

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -15,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +23,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 var express = require('express');
+
+var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
+var _ = require('underscore');
+
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Compare two products from Lazada' });
+  res.render('index', { title: 'Compare products' });
 });
+
+/* GET products JSON */
+router.get('/shards/products', function(req, res, next) {
+    async.map(req.query.url || [], loadUrl, function(err, result) {
+    	if (err) throw err;
+    	res.json(result);
+    });
+});
+
+
+function loadUrl(url, callback) {
+    var options = {
+       url: url,
+       headers: {
+           'User-Agent': 'Mozilla/5.0 (Macintosh)'
+       }
+    };
+
+    request(options, function(err, res, html) {
+        if (!err && res.statusCode == 200) {
+            callback(null, parseData(html));
+        } else {
+            callback('Server responded with ' + res.statusCode + ' status.');
+        }
+    });
+}
+
+function parseData(data) {
+    var $ = cheerio.load(data);
+
+	var result = {
+		name: $('h1').text().trim(),
+		review_count: $('#review a').text().trim(),
+		price: $('#price_box').text().trim(),
+		price_special: $('#special_price_box').text().trim(),
+		image: $('#productImageBox > .productImage').data('image'),
+		href: $('link[rel=canonical]').attr('href'),
+		specs: {}
+	};
+
+	$('.specification-table tr').each(function(index, elem) {
+	    var $cells = $(elem).children(),
+	        name = $cells.first().text().trim(),
+            value = $cells.last().text().trim();
+
+        if (name && value) {
+            result.specs[name] = value;
+        }
+	});
+
+	var rating = $('#rating .itm-ratRating').css('width');
+	if (rating) {
+		result.rating = rating.replace(/\D*/g, '');
+	}
+
+	return result;
+}
+
 
 module.exports = router;
